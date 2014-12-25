6c912c5b98480e6798a1a04978698684795acaf3
wallace <wallace.dyfq@gmail.com>
2014-12-24 19:10:21 -0700

    initial commit

---
+++ Matrix.cc
@@ -0,0 +1,126 @@
+#include "Matrix.hh"
+#include<cassert>
+#include<iostream>
+
+Matrix::Matrix():row(0), col(0) {
+  
+}
+
+
+Matrix::Matrix(int r, int c): row(r), col (c) {
+
+  elem = new int[row*col];
+  for (int rr = 0; rr < row; ++rr)
+    for (int cc = 0; cc < col; ++cc)
+      setelem(rr, cc, 0);
+
+}
+
+Matrix::Matrix( const Matrix &aa) {
+
+  copy(aa);
+    
+}
+
+Matrix::~Matrix() {
+  cleanup();
+
+}
+
+int Matrix::getrows() const {
+  return row;
+}
+
+int Matrix::getcols() const {
+  return col;
+}
+
+int Matrix::getelem(int r, int c) const {
+
+  return elem[r*col + c];
+}
+
+void Matrix::setelem(int r, int c, int number)
+{
+    elem[r * col + c] = number;
+}
+
+
+void Matrix::add(Matrix &aa) {
+  assert(aa.getrows() == row);
+  assert(aa.getcols() == col);
+  for (int rr = 0; rr < row; ++rr)
+    for (int cc = 0; cc < col; ++cc)
+      elem[rr * col + cc] += aa.getelem(rr, cc);
+
+}
+  
+
+void Matrix::subtract(Matrix &aa) {
+  assert(aa.getrows() == row);
+  assert(aa.getcols() == col);
+  for (int rr = 0; rr < row; ++rr)
+    for (int cc = 0; cc < col; ++cc)
+
+      elem[rr * col + cc] -= aa.getelem(rr, cc);
+
+}
+
+
+bool Matrix::equals(const Matrix &aa)  {
+  assert(aa.getrows() == row);
+  assert(aa.getcols() == col);
+  for (int rr = 0; rr < row; ++rr)
+    for (int cc = 0; cc < col; ++cc) {
+      
+      if (elem[rr * col + cc] != aa.getelem(rr, cc))
+        return false;
+    }
+  return true;
+
+}
+  
+
+Matrix& Matrix::operator= (const Matrix &aa) {
+
+  std::cout<<"call operator ="<<std::endl;
+  if(this != &aa) {
+    if (row !=0 || col != 0) 
+      delete[] elem;
+    
+    row = aa.getrows();
+    std:: cout<< "aa rows = "<<aa.getrows()<<std::endl;
+    col = aa.getcols();
+    std::cout<< "aa cols = "<<aa.getcols()<<std::endl;
+    
+    elem = new int[row * col];
+    for (int rr = 0; rr < row; ++rr)
+      for (int cc = 0; cc < col; ++cc) {
+        int tmp = aa.getelem(rr, cc);
+        setelem(rr, cc, tmp);
+        // elem[rr*col + cc] = tmp;
+    }
+  }
+  return *this;
+}
+
+void Matrix::cleanup() {
+   if (row !=0 || col !=0)
+    delete[] elem ;
+}
+
+void Matrix::copy(const Matrix & aa) {
+  row = aa.getrows();
+  col = aa.getrows();
+  int elenumber = row * col;
+
+  elem = new int[elenumber];
+
+  for (int rr = 0; rr < row; ++rr)
+    for (int cc = 0; cc < col; ++cc) {
+      //    elem[col*rr + cc] = aa.getelem(rr, cc);
+      int tmp = aa.getelem(rr, cc);
+      setelem(rr, cc, tmp);
+    }
+
+}
---
+++ Matrix.hh
@@ -0,0 +1,34 @@
+class Matrix{
+ private:
+  int row, col;
+  int *elem;
+  void  copy(const Matrix &aa);
+  void cleanup();
+ public:
+  // Constructor
+
+  Matrix(); //default constructor create a 0 by 0 matrix
+  Matrix(int r, int c);
+  Matrix(const Matrix &aa);
+
+  // Destructor
+  ~Matrix();
+
+  //getters
+  int getrows() const;
+  int getcols() const;
+  int getelem(int r, int c)const ;
+
+  //setters
+  void setelem(int r, int c, int number);
+
+  // Mutator method
+  void add(Matrix &aa);
+  void subtract(Matrix &aa);
+  bool equals(const Matrix &aa) ;
+
+  // operator
+  Matrix& operator=(const  Matrix &aa);
+
+
+};
